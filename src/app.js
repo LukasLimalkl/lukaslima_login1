de0ff7a4b4e07644f709768209cdda1b94fa3568
LukasLima63 <lukas.lima@portallacrei.com.br>
2023-11-12 15:11:09 -0300

    adding swagger

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = require('./router');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 app.use(router);
+app.use;
 
 module.exports = app;
